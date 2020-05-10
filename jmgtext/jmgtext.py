60aca7f48cc2f0d1f30497198c164ea35c47e4f7
James Gloudemans <james.gloudemans@gmail.com>
2020-05-10 19:16:04 -0400

    Handles arbitrary insertion pos

--- jmgtext/jmgtext.py
+++ jmgtext/jmgtext.py
@@ -6,29 +6,33 @@ import string
 # third party libraries
 
 # local modules
+from rope import Rope
 
 
 def main(stdscr):
     """Main text editor loop."""
     buffer = ""
+    str_pos = 0
     while True:
         key = stdscr.getkey()
-        pos = stdscr.getyx()
+        cur_pos = stdscr.getyx()
 
         if len(key) == 1 and curses.unctrl(key) == b"^P":
             stdscr.addstr(10, 0, buffer)
         elif key in string.printable:
-            buffer += key
+            buffer = buffer[:str_pos] + key + buffer[str_pos:]
+            str_pos += 1
         elif key == "KEY_BACKSPACE":
-            buffer = buffer[:-1]
-            if pos[1] == 0:
-                if pos[0] != 0:
+            buffer = buffer[: str_pos - 1] + buffer[str_pos:]
+            str_pos -= 1
+            if cur_pos[1] == 0:
+                if cur_pos[0] != 0:
                     x = len(buffer.splitlines()[-1])
-                    stdscr.move(pos[0] - 1, x)
+                    stdscr.move(cur_pos[0] - 1, x)
             else:
-                pos = (pos[0], pos[1] - 1)
-                stdscr.move(*pos)
-                stdscr.delch(*pos)
+                cur_pos = (cur_pos[0], cur_pos[1] - 1)
+                stdscr.move(*cur_pos)
+                stdscr.delch(*cur_pos)
         else:
             pass
 
