6ec9be75de7950c875ec0080b0c87dba8de62842
James Gloudemans <james.gloudemans@gmail.com>
2020-05-10 21:04:35 -0400

    Implement key_left, many bugfixes

--- jmgtext/jmgtext.py
+++ jmgtext/jmgtext.py
@@ -9,34 +9,50 @@ import string
 from rope import Rope
 
 
-def main(stdscr):
+def main(screen):
     """Main text editor loop."""
     buffer = ""
     str_pos = 0
     while True:
-        key = stdscr.getkey()
-        cur_pos = stdscr.getyx()
+        key = screen.getkey()
+        cur_pos = screen.getyx()
 
-        if len(key) == 1 and curses.unctrl(key) == b"^P":
-            stdscr.addstr(10, 0, buffer)
-        elif key in string.printable:
-            buffer = buffer[:str_pos] + key + buffer[str_pos:]
-            str_pos += 1
+        if len(key) == 1 and curses.unctrl(key) == b"^X":
+            break
+        elif len(key) == 1 and curses.unctrl(key) == b"^P":
+            screen.addstr(10, 0, buffer)
         elif key == "KEY_BACKSPACE":
             buffer = buffer[: str_pos - 1] + buffer[str_pos:]
-            str_pos -= 1
             if cur_pos[1] == 0:
                 if cur_pos[0] != 0:
                     x = len(buffer.splitlines()[-1])
-                    stdscr.move(cur_pos[0] - 1, x)
+                    cur_pos = (cur_pos[0] - 1, x)
+                    str_pos -= 1
             else:
                 cur_pos = (cur_pos[0], cur_pos[1] - 1)
-                stdscr.move(*cur_pos)
-                stdscr.delch(*cur_pos)
+                screen.delch(*cur_pos)
+                str_pos -= 1
+        elif key == "KEY_LEFT":
+            if cur_pos[1] == 0:
+                if cur_pos[0] != 0:
+                    x = len(buffer.splitlines()[-1])
+                    cur_pos = (cur_pos[0] - 1, x)
+                    str_pos -= 1
+            else:
+                cur_pos = (cur_pos[0], cur_pos[1] - 1)
+                str_pos -= 1
+        elif key in string.printable:
+            buffer = buffer[:str_pos] + key + buffer[str_pos:]
+            if key == "\n":
+                cur_pos = (cur_pos[0] + 1, 0)
+            else:
+                cur_pos = (cur_pos[0], cur_pos[1] + 1)
+            str_pos += 1
         else:
             pass
 
-        stdscr.addstr(0, 0, buffer)
+        screen.addstr(0, 0, buffer)
+        screen.move(*cur_pos)
 
 
 if __name__ == "__main__":
