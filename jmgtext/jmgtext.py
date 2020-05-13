ccbfd8a5f087d8b25ee3cf882d6b7b1dac1c239f
James Gloudemans <james.gloudemans@gmail.com>
2020-05-12 20:27:05 -0400

    Refactored, simplified main()

--- jmgtext/jmgtext.py
+++ jmgtext/jmgtext.py
@@ -9,50 +9,61 @@ import string
 from rope import Rope
 
 
+def insert(s: str, i: int, sub: str) -> str:
+    """Shortcut to insert sub at position i in s."""
+    return s[:i] + sub + s[i:]
+
+
+def remove(s: str, i: int) -> str:
+    """Shortcut to remove character i from s."""
+    return s[: i - 1] + s[i:]
+
+
 def main(screen):
     """Main text editor loop."""
-    buffer = ""
-    str_pos = 0
+    buffer = [""]
     while True:
         key = screen.getkey()
-        cur_pos = screen.getyx()
-
+        y, x = screen.getyx()
         if len(key) == 1 and curses.unctrl(key) == b"^X":
             break
         elif len(key) == 1 and curses.unctrl(key) == b"^P":
-            screen.addstr(10, 0, buffer)
+            screen.addstr(10, 0, "\n".join(buffer))
         elif key == "KEY_BACKSPACE":
-            buffer = buffer[: str_pos - 1] + buffer[str_pos:]
-            if cur_pos[1] == 0:
-                if cur_pos[0] != 0:
-                    x = len(buffer.splitlines()[cur_pos[0] - 1])
-                    cur_pos = (cur_pos[0] - 1, x)
-                    str_pos -= 1
+            if x == 0:
+                if y != 0:
+                    x = len(buffer[y - 1])
+                    buffer[y - 1] += buffer[y]
+                    buffer[y:] = buffer[y + 1 :]
+                    buffer[-1] = ""
+                    screen.clear()
+                    y -= 1
             else:
-                cur_pos = (cur_pos[0], cur_pos[1] - 1)
-                screen.delch(*cur_pos)
-                str_pos -= 1
+                buffer[y] = remove(buffer[y], x)
+                x -= 1
+                screen.delch(y, x)
         elif key == "KEY_LEFT":
-            if cur_pos[1] == 0:
-                if cur_pos[0] != 0:
-                    x = len(buffer.splitlines()[cur_pos[0] - 1])
-                    cur_pos = (cur_pos[0] - 1, x)
-                    str_pos -= 1
+            if x == 0:
+                if y != 0:
+                    y -= 1
+                    x = len(buffer[y])
             else:
-                cur_pos = (cur_pos[0], cur_pos[1] - 1)
-                str_pos -= 1
+                x -= 1
+
         elif key in string.printable:
-            buffer = buffer[:str_pos] + key + buffer[str_pos:]
             if key == "\n":
-                cur_pos = (cur_pos[0] + 1, 0)
+                buffer.insert(y + 1, buffer[y][x:])
+                buffer[y] = buffer[y][:x]
+                y += 1
+                x = 0
             else:
-                cur_pos = (cur_pos[0], cur_pos[1] + 1)
-            str_pos += 1
+                buffer[y] = insert(buffer[y], x, key)
+                x += 1
         else:
             pass
 
-        screen.addstr(0, 0, buffer)
-        screen.move(*cur_pos)
+        screen.addstr(0, 0, "\n".join(buffer))
+        screen.move(y, x)
 
 
 if __name__ == "__main__":
